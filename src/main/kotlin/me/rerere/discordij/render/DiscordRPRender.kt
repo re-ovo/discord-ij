fdfc20eb05fb686f120dca1a3368e77f9a796061
re-ovo <qq1609403959@outlook.com>
2023-03-15 08:18:56 +0800

    Fixed the error when launching intellij without discord running

--- src/main/kotlin/me/rerere/discordij/render/DiscordRPRender.kt
+++ src/main/kotlin/me/rerere/discordij/render/DiscordRPRender.kt
@@ -2,20 +2,24 @@ package me.rerere.discordij.render
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
+import de.jcm.discordgamesdk.ActivityManager
 import de.jcm.discordgamesdk.Core
 import de.jcm.discordgamesdk.CreateParams
 import de.jcm.discordgamesdk.activity.Activity
 import kotlinx.coroutines.*
 import me.rerere.discordij.APPLICATION_ID
+import me.rerere.discordij.DiscordIJ
 import java.util.*
 
 @Service
 class DiscordRPRender : Disposable {
+    private lateinit var activityManager: ActivityManager
     private val scope = CoroutineScope(
         Dispatchers.Default + SupervisorJob()
     )
 
-    private val activityManager by lazy {
+    // TODO: Maybe let use to re-init discord rp if they want
+    fun init() = kotlin.runCatching {
         val core = Core(
             CreateParams().apply {
                 clientID = APPLICATION_ID
@@ -26,10 +30,16 @@ class DiscordRPRender : Disposable {
             delay(1000L)
             core.runCallbacks()
         }
-        core.activityManager()
+        activityManager = core.activityManager()
+    }.also {
+        DiscordIJ.logger.info("DiscordRP init result: ${it.isSuccess}")
     }
 
-    fun updateActivity(activity: ActivityWrapper) {
+    init {
+        init()
+    }
+
+    fun updateActivity(activity: ActivityWrapper) = kotlin.runCatching {
         val activityNative = Activity()
         activityNative.state = activity.state
         activityNative.details = activity.details
@@ -44,11 +54,16 @@ class DiscordRPRender : Disposable {
         activityNative.assets().smallImage = activity.smallImageKey
         activityNative.assets().smallText = activity.smallImageText
         scope.launch(Dispatchers.IO) {
-            activityManager.updateActivity(activityNative)
+            kotlin.runCatching {
+                activityManager.updateActivity(activityNative)
+            }
+                .onFailure {
+                    DiscordIJ.logger.warn("Failed to update activity: " + it.message)
+                }
         }
     }
 
-    fun clearActivity() {
+    fun clearActivity() = kotlin.runCatching {
         activityManager.clearActivity()
     }
 
